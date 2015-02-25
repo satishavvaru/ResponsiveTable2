9fee9ae50083074497c63f1ef56c210135911117
satishavvaru <sateesh.avvaru@gmail.com>
2015-02-25 17:48:33 +0530

    ResponsiveTable2
    Test

---
+++ MVC_ResponsiveGrid3/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_ResponsiveGrid3.Models;
+
+namespace MVC_ResponsiveGrid3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        ProtoEntities db = new ProtoEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+	}
+}
